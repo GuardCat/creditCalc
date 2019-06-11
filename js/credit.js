b7a7972984f904cfecdaf9b1187e6d0a2d1a688b
Либин Николай Павлович <N.Libin@isb>
2019-06-11 11:13:49 +0300

    Добавлена возможность рассчитывать погашение большим платежом. Но пока неверно считается переплата и общая сумма выплат для таких случаев.

--- js/credit.js
+++ js/credit.js
@@ -8,32 +8,38 @@
  * @return	{object} объект, содержащий переплату, суммы выплат и расписание погашения. 
  */
 
-function calcuateAnnuitet (sum, rate, term) {
-	const 
-		monthlyRate			= rate / 100 / 12,
-		monthlyPayment		= +( (sum * ( monthlyRate + monthlyRate / ( Math.pow( (1 + monthlyRate), term ) - 1) )).toFixed(2) ),
-		totalSum			= monthlyPayment * term,
-		overpaySum			= totalSum - sum
-	;
+function calculateAnnuitetPayment(sum, monthlyRate, term) {
+	return + ( (sum * ( monthlyRate + monthlyRate / ( Math.pow( (1 + monthlyRate), term ) - 1) ) ).toFixed(2) );
+}
+
+function calcuateAnnuitet (sum, rate, term, myPayment = 0) {
+	const monthlyRate = rate / 100 / 12;
+	
+	let	monthlyPayment = calculateAnnuitetPayment(sum, monthlyRate, term);
+	
+	const totalSum = monthlyPayment * term, overpaySum = totalSum - sum;
 	
 	let 
 		result = { 
-			shedule:		[ ], 
-			totalSum:		totalSum,
-			overpayment:	{
-				sum:		overpaySum,
-				percent:	+( (overpaySum / sum * 100).toFixed(2) )
+			shedule: [ ], 
+			totalSum: totalSum,
+			overpayment: {
+				sum: overpaySum,
+				percent: +( (overpaySum / sum * 100).toFixed(2) )
 			}
 		},
-		remainingSum 		= sum,
+		remainingSum = sum,
 		monthlyOverpaySum,
 		monthlyDebtSum
 	;
 	
 	for (let i = 0; i < term; i++) {
 		monthlyOverpaySum	= remainingSum * monthlyRate;
-		monthlyDebtSum		= monthlyPayment - monthlyOverpaySum;
+		monthlyPayment		= myPayment ? calculateAnnuitetPayment(remainingSum, monthlyRate, term - i) : monthlyPayment;
+		monthlyDebtSum		= monthlyPayment - monthlyOverpaySum - ( myPayment ? (monthlyPayment - myPayment) : 0 );
 		remainingSum		-=monthlyDebtSum;
+
+		if (remainingSum <= 0) break;
 
 		result.shedule.push( {payment: monthlyPayment, debt: monthlyDebtSum, overpay: monthlyOverpaySum } );
 	}
