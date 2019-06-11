4a322d91fb0db62664fb4ca8265827bed8b6c7eb
Либин Николай Павлович <N.Libin@isb>
2019-06-11 12:03:26 +0300

    Добавили возможность вводить массив ручных платежей. Обнаружена ошибка: рассчитываем сумму платежа слишком рано (опоздание на месяц), нужно поменять логику

--- js/credit.js
+++ js/credit.js
@@ -1,48 +1,43 @@
 /*jshint es5: true, esnext: true, browser: true*/
 
-/**
- * 
- * @param	{number} sum сумма кредита в рублях
- * @param	{number} rate годовая ставка в процентах (0—100)
- * @param	{number} term ставка в месяцах
- * @return	{object} объект, содержащий переплату, суммы выплат и расписание погашения. 
- */
+
 
 function calculateAnnuitetPayment(sum, monthlyRate, term) {
 	return + ( (sum * ( monthlyRate + monthlyRate / ( Math.pow( (1 + monthlyRate), term ) - 1) ) ).toFixed(2) );
 }
 
-function calcuateAnnuitet (sum, rate, term, myPayment = 0) {
+/**
+ * 
+ * @param	{number}	sum сумма кредита в рублях
+ * @param	{number}	rate годовая ставка в процентах (0—100)
+ * @param	{number}	term ставка в месяцах
+ * @return	{array}		массив, содержащий инфо о каждом платеже
+ * @requires			function calculateAnnuitetPaymen
+ * 
+ */
+function generateAnnuitetShedule (sum, rate, term, customPayments = [ ]) {
 	const monthlyRate = rate / 100 / 12;
 	
-	let	monthlyPayment = calculateAnnuitetPayment(sum, monthlyRate, term);
-	
-	const totalSum = monthlyPayment * term, overpaySum = totalSum - sum;
-	
-	let 
-		result = { 
-			shedule: [ ], 
-			totalSum: totalSum,
-			overpayment: {
-				sum: overpaySum,
-				percent: +( (overpaySum / sum * 100).toFixed(2) )
-			}
-		},
+	let
+		monthlyPayment = calculateAnnuitetPayment(sum, monthlyRate, term),
 		remainingSum = sum,
 		monthlyOverpaySum,
-		monthlyDebtSum
+		monthlyDebtSum,
+		customPayment = false,
+		shedule = [ ]
 	;
 	
 	for (let i = 0; i < term; i++) {
+		customPayment		= customPayments[i] ? customPayments[i] : false;
 		monthlyOverpaySum	= remainingSum * monthlyRate;
-		monthlyPayment		= myPayment ? calculateAnnuitetPayment(remainingSum, monthlyRate, term - i) : monthlyPayment;
-		monthlyDebtSum		= monthlyPayment - monthlyOverpaySum - ( myPayment ? (monthlyPayment - myPayment) : 0 );
+		monthlyPayment		= customPayment ? calculateAnnuitetPayment(remainingSum, monthlyRate, term - i) : monthlyPayment;
+		monthlyDebtSum		= monthlyPayment - monthlyOverpaySum - ( customPayment ? (monthlyPayment - customPayment) : 0 );
 		remainingSum		-=monthlyDebtSum;
 
 		if (remainingSum <= 0) break;
 
-		result.shedule.push( {payment: monthlyPayment, debt: monthlyDebtSum, overpay: monthlyOverpaySum } );
+		shedule.push( {payment: monthlyPayment, debt: monthlyDebtSum, overpay: monthlyOverpaySum } );
 	}
 	
-	return result;
+	return shedule;
 }
