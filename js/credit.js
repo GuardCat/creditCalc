cfe12e626b219bc6888ec3abc5d9f0d8867f65cf
GuardCat <storktec@mail.ru>
2020-09-25 10:21:09 +0300

    Расчет графика готов. Переплата в графике совпадает с ТКС. Переплата общая --- нет, мы считаем по сумме платежей, ТКС --- по коэффициенту аннуитета

--- js/credit.js
+++ js/credit.js
@@ -1,37 +1,62 @@
 /*jshint esversion: 8, browser: true*/
 
-"use stricrt"
+"use strict"
 
 /**
  *
  * @param	{number}	sum сумма кредита в рублях
  * @param	{number}	rate ставка
  * @param	{number}	term срок в месяцах
- * @requires {function} annuitetCoefficient
  * @return	{number}	ежемесячный платеж
  *
  */
 function annuitetPayment(sum, rate, term) {
-	return +( ( sum * annuitetCoefficient(rate, term) ).toFixed(2) );
-}
-
-function annuitetCoefficient(rate, term) {
-  const mRate = rate / 12 / 100;
-  return ( mRate * Math.pow(mRate + 1, term) / ( Math.pow(mRate + 1, term) - 1 ) );
+	const
+		mRate = rate / 12 / 100,
+		annuitetCoefficient = mRate * Math.pow(mRate + 1, term) / ( Math.pow(mRate + 1, term) - 1 )
+	;
+	return +( (sum * annuitetCoefficient).toFixed(2) );
 }
 
 
 /**
- *
- * @param	{number}	sum сумма кредита в рублях
- * @param	{number}	rate годовая ставка в процентах (0—100)
- * @param	{number}	term ставка в месяцах
- * @requires			function calculateAnnuitetPayment
+ * @param		{number}	sum сумма кредита в рублях
+ * @param		{number}	rate годовая ставка в процентах (0—100)
+ * @param		{number}	term ставка в месяцах
+ * @requires	{function}	annuitetPayment
+ * @requires	{function}	datesDiff
  *
  */
 function generateSchedule (sum, rate, term, date = new Date( ) ) {
-  const 
-	return schedule;
+	const dailyRate = rate / 365 / 100;
+	let 
+		payment = annuitetPayment(sum, rate, term),
+		schedule = [ ],	summary = { totalSum: 0, overpay: 0, overpayProc: 0},
+		paymentDate = date,	previousDate,
+		debt = sum, overpay
+	;
+	
+	for (let i = 0; i < term; i++ ) {
+		previousDate = new Date(paymentDate);
+		paymentDate.setMonth( paymentDate.getMonth( ) + 1 );
+		
+		if (debt < payment) payment = debt;
+		overpay = debt * dailyRate * datesDiff(previousDate, paymentDate)
+		schedule.push( {date: new Date (paymentDate) , sum: payment, debtSum: payment - overpay, overpay} );
+		summary.totalSum += payment;
+		debt -= payment - overpay;
+	}
+	
+	summary.overpay = summary.totalSum - sum;
+	summary.overpayProc = +( (summary.overpay /  sum * 100).toFixed(2) )
+	
+	return {schedule, summary};
+}
+
+function datesDiff(d1, d2) {
+	if ( !d1 || !(d1 instanceof Date) || isNaN(d1) ) return new Error(`function datesDiff: incorrect date1 (${d1}) `);
+	if ( !d2 || !(d2 instanceof Date) || isNaN(d2) ) return new Error(`function datesDiff: incorrect date2 (${d2}) `);
+	return Math.ceil( (d2 - d1) / (60 * 60 * 24 * 1000) );
 }
 
 function getDaysInMonth(year, month) {
